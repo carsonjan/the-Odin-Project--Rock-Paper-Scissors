58f2c5df5ad3cd5ebb04614d27ac2c54d06559e7
Carson Jan <sc.carson.jan@gmail.com>
2025-09-16 23:07:32 -0700

    Step 2: Implement getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Hello World');
+
+function getComputerChoice() {
+    randomNum = Math.random();
+    if (randomNum < 1/3) return "rock";
+    if (randomNum > 2/3) return "paper";
+    return "scissors";
+}
