7b9a19260f34846209b4ab0462c240721461b2d0
Carson Jan <sc.carson.jan@gmail.com>
2025-09-16 23:17:59 -0700

    Step 3: Implement get human choice

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 console.log('Hello World');
 
 function getComputerChoice() {
-    randomNum = Math.random();
+    const randomNum = Math.random();
     if (randomNum < 1/3) return "rock";
     if (randomNum > 2/3) return "paper";
     return "scissors";
+}
+
+function getHumanChoice() {
+    while (true) {
+        const input = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        if (["rock", "paper", "scissors"].includes(input)) return input;
+    }
 }
