9153d2b863400ca39f053ce36ab1e5a8b188204b
Carson Jan <sc.carson.jan@gmail.com>
2025-09-16 23:34:47 -0700

    Step 5: Implement game logic

--- script.js
+++ script.js
@@ -15,3 +15,50 @@ function getHumanChoice() {
         if (["rock", "paper", "scissors"].includes(input)) return input;
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("Tie!");
+        return;
+    }
+    if (humanChoice == "rock") {
+        switch (computerChoice) {
+            case "paper":
+                console.log("You Lose! Paper Beats Rock.")
+                humanScore -= 1;
+                computerScore += 1;
+                break;
+            case "scissors":
+                console.log("You Win! Rock Beats Scissors.")
+                humanScore += 1;
+                computerScore -= 1;
+                break;
+        }
+    } else if (humanChoice == "paper") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You Win! Paper Beats Rock.");
+                humanScore += 1;
+                computerScore -= 1;
+                break;
+            case "scissors":
+                console.log("You Lose! Scissors Beats Paper.");
+                humanScore -= 1;
+                computerScore += 1;
+                break;
+        }
+    } else if (humanChoice == "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You Lose! Rock Beats Scissors.");
+                humanScore -= 1;
+                computerScore += 1;
+                break;
+            case "paper":
+                console.log("You Win! Scissors Beats Paper.");
+                humanScore += 1;
+                computerScore -= 1;
+                break;
+        }
+    }
+} 
